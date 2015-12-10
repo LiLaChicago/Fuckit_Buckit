ce3f6479f30e0417d0b12036850f3b46084a3152
Lidia Santos <Lidia@Lidias-MacBook-Pro.local>
2015-12-10 17:44:30 -0600

    datatable

--- db/migrations.sql
+++ db/migrations.sql
@@ -4,7 +4,7 @@ CREATE DATABASE buckit;
 
 CREATE TABLE accounts (id SERIAL PRIMARY KEY, name VARCHAR(255), user_name VARCHAR(255), user_email VARCHAR(255), password_digest VARCHAR(255), user_name_id INTEGER);
 
-CREATE TABLE buckits (id SERIAL PRIMARY KEY, buck TEXT, user_name_id INTEGER, user_image_id INTEGER);
+CREATE TABLE buckits (id SERIAL PRIMARY KEY, buck INTEGER, user_name_id INTEGER, user_image_id INTEGER);
 
 CREATE TABLE user_images (id SERIAL PRIMARY KEY, user_name_id INTEGER, image_base64 TEXT);
 
